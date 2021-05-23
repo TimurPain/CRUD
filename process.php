7cdf539c33d215b1d40cf4772af51f7da3b014bd
TimurPain <84668346+TimurPain@users.noreply.github.com>
2021-05-23 18:37:18 +0600

    Add files via upload

---
+++ process.php
@@ -0,0 +1,47 @@
+<?php 
+
+$id = 0;
+$name = '';
+$location = '';
+$update = false;
+
+$mysqli = new mysqli('localhost', 'mysql', 'mysql', 'crud') or die(mysqli_error($mysqli));
+
+if (isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or die($mysqli->error);
+
+    header('location: index.php');
+}
+
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
+
+    $_SESSION['message'] = "Record has been deleted!";
+    $_SESSION['msg_type'] = "danger";
+}
+
+if (isset($_GET['edit'])) {
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
+    if ($result == 1) {
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $location = $row['location'];
+    }
+}
+
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id=$id") or die($mysqli->error);
+
+    header('location: index.php');
+}
+?>
