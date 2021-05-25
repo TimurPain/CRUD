3c6d94fdf9e838a5d2ad4d4368ae14629fc3ac6b
Timur Tanyrbergenov <84668346+TimurPain@users.noreply.github.com>
2021-05-25 21:39:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <title>CRUD</title>
   </head>
 
@@ -33,10 +34,19 @@
       $mysqli = new mysqli('localhost', 'mysql', 'mysql', 'crud') or die(mysqli_error($mysqli));
       $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
     ?>
-
+        
      <div>
+          <p><h5>Добавить пользователя<a href="add.php" style="color: black">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+          </svg></a></h5></p>
       <table class="table">
         <thead>
+          <tr>
+            <th><h2>Работники</h2></th>
+            <th><h4>Поиск</h4><input type="text" name="" value="" class="form-control" placeholder="Поиск..."></th>
+            <th><button type="sumbit" class="btn btn-primary" name="search">-></button></th>
+          </tr>
           <tr>
             <th>Name</th>
             <th>Location</th>
@@ -90,7 +100,6 @@
         <?php endif; ?>
         </div>
     </form>
-    </div>
     </div>
   </body>
 </html>
